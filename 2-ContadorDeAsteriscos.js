d88001dda8dd0cb182a5818d4ae04bad88dcc060
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-09-08 10:47:31 -0300

    Exibindo asteriscos utilizando dois loops for

--- 2-ContadorDeAsteriscos.js
+++ 2-ContadorDeAsteriscos.js
@@ -4,11 +4,18 @@
 exibirAsteriscos(10);
 function exibirAsteriscos(linhas) {
 
- let asterisco = '';
+//  let asterisco = '';
 
- for (let index = 0; index <= linhas; index++) 
- {
-     asterisco += '*'
-     console.log(index, asterisco)
- }
+//  for (let index = 0; index <= linhas; index++) 
+//  {
+//      asterisco += '*'
+//      console.log(index, asterisco)
+//  }
+    for ( let index = 0; index <= linhas; index++) {
+     let asterisco = '';
+        for ( let indexAsterisco = 0; indexAsterisco <= index; indexAsterisco++) {
+        asterisco += '*';
+    }
+    console.log(index, asterisco)
+}
 }
