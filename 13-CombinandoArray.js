d79266b9cc6ff360f8842d304e8e0af60f91aa9a
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-09-20 11:43:55 -0300

    Utilizando os métodos: join e split

---
+++ 13-CombinandoArray.js
@@ -0,0 +1,12 @@
+//Método join()
+//Método split()
+let numeros = [1,2,3,4,5]
+
+const combinado = numeros.join('.');
+console.log(combinado)
+
+const frase = 'olá, seja bem vindo !';
+const resultado = frase.split(' ');
+const resultadoPorPalavra = frase.split('');
+console.log(resultado)
+console.log(resultadoPorPalavra)
